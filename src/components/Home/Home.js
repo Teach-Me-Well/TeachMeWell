be765231faa762de8c4902239a1b536da6f5576e
Mayur Bhargava <mayurbhargava75@gmail.com>
2023-02-22 23:11:06 +0530

    magva

---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+ 
+
+// import HeadInfo from '../headinfo/HeadInfo'
+import Navbar from "../Navbar/Navbar";
+
+// import Annocument from '../Annocument'
+// import Wrapper from '../wrapper/Wrapper'
+// import Footer from '../footer/Footer'
+// import Slideingimg from '../slideingimg/slidingimg'
+// import Form from '../form/Form'
+
+const Home = () => {
+
+  return (
+    <div className="container">
+
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React    from "react";
+const Navbar = () => {
+
+
+  return (
+  <div>
+    <h1>Hello MAGVA</h1>
+  </div>
+  );
+};
+
+export default Navbar;
